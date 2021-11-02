665639f002e939a842f65f139c7f4e62959a7a41
kamilix2003 <kamaj2309@gmail.com>
2021-11-02 20:30:35 +0100

    Update file.bat

--- file.bat
+++ file.bat
@@ -1,1 +1,9 @@
-start C:\Users\kamil\desktop\reklama.bmp 
+@echo off
+cd %userprofile%
+
+curl https://i.imgur.com/g2bOBiW.jpg -o reklama.bmp
+
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %userprofile%\reklama.bmp /f
+
+RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
