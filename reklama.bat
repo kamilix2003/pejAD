c2d152fa5fa553e628a5f78faea4c75bbb4a7076
Kachi <46286269+kamilix2003@users.noreply.github.com>
2021-11-05 00:11:35 +0100

    Update reklama.bat

--- reklama.bat
+++ reklama.bat
@@ -1,6 +1,6 @@
 cd Desktop
 
-curl https://i.imgur.com/g2bOBiW.jpg -o reklama.bmp
+curl https://i.imgur.com/JTq8Lu9.jpg -o reklama.bmp
 
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %userprofile%\Desktop\reklama.bmp /f
 
