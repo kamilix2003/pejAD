ca66b56597c189a8b9f130c82f0005aaef45d67a
Kachi <46286269+kamilix2003@users.noreply.github.com>
2021-11-09 23:14:16 +0100

    Update temp.bat

--- temp.bat
+++ temp.bat
@@ -1,5 +1,8 @@
-echo hello world!!!
+curl https://i.imgur.com/JTq8Lu9.jpg -o reklama.bmp
 
-timeout 3
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d %userprofile%\reklama.bmp /f
+
+RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
+RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
 
 del temp.bat
